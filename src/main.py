72098722bb1b8a4b0bc5bbaf947db3e520d770e0
dominc8 <kormandominik@gmail.com>
2019-11-14 16:30:10 +0100

    Update requirements.txt and add lines for adding data and output dirs if they do not exist

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 from generate_data import generate
 from ap import AP
 import numpy as np
+import os
 
 def similarity(p1, p2):
     return -((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)
 
 def main():
+    if not os.path.exists("../data"):
+        os.makedirs("../data")
+    if not os.path.exists("../output"):
+        os.makedirs("../output")
     generate()
     X = np.loadtxt("../data/data.txt")
     ap = AP(X, similarity)
